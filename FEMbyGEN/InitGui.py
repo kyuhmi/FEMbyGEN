c8539f78d4d29f2ca6918c09446928954c36632e
OgedaYY <104031087+OgedaYY@users.noreply.github.com>
2022-08-12 17:45:03 +0300

    Add files via upload
    Changing mode name

---
+++ FEMbyGEN/InitGui.py
@@ -0,0 +1,37 @@
+import FreeCADGui
+
+class FGWorkbench(Workbench):
+    MenuText = "FEMbyGEN"
+    ToolTip = "Use generative design to optimise your part for Additive Manufacturing"
+    Icon = FreeCAD.getUserAppDataDir() + "Mod/FEMbyGEN/icon.svg"
+
+    def Initialize(self):
+        """This function is executed when FreeCAD starts"""
+        #import MyModuleA, MyModuleB  # import here all the needed files that create your FreeCAD commands
+        import Initiate, Alias, Generate, FEA, Results, Refine
+        FreeCADGui.addIconPath(FreeCAD.getUserAppDataDir() + "Mod/FEMbyGEN/")
+        self.list = ["Initiate", "Alias","Generate", "FEA", "Refine", "Results"]  # A list of command names created in the line above
+        self.appendToolbar("Commands", self.list)  # creates a new toolbar with your commands
+        self.appendMenu("FEMbyGEN", self.list)  # creates a new menu
+
+
+    def Activated(self):
+        """This function is executed when the workbench is activated"""
+        return
+
+    def Deactivated(self):
+        """This function is executed when the workbench is deactivated"""
+        return
+
+    def ContextMenu(self, recipient):
+        """This is executed whenever the user right-clicks on screen"""
+        # "recipient" will be either "view" or "tree"
+        self.appendContextMenu("My commands", self.list)  # add commands to the context menu
+
+    def GetClassName(self):
+        # This function is mandatory if this is a full python workbench
+        # This is not a template, the returned string should be exactly "Gui::PythonWorkbench"
+        return "Gui::PythonWorkbench"
+
+
+Gui.addWorkbench(FGWorkbench())
---
+++ FEMbyGEN/Initiate.py
@@ -0,0 +1,43 @@
+import FreeCAD, FreeCADGui, Part, Mesh
+import PySide
+
+
+class InitiateCommand():
+    """Analyse the generated parts"""
+
+    def GetResources(self):
+        return {'Pixmap'  : 'Initiate.svg',  # the name of a svg file available in the resources
+                'Accel' : "Shift+N",  # a default shortcut (optional)
+                'MenuText': "Initiate",
+                'ToolTip' : "Initialise the generation process"}
+
+    def Activated(self): 
+        return InitiatePanel()
+    
+    def IsActive(self):
+        """Here you can define if the command must be active or not (greyed) if certain conditions
+        are met or not. This function is optional."""
+        return True
+
+class InitiatePanel:
+    def __init__(self):   
+        # Add spreatsheet
+        self.paramsheet=FreeCAD.activeDocument().addObject('Spreadsheet::Sheet','Parameters')
+        # Spreadsheet editing
+        for i in range(10):
+            self.paramsheet.set(f'A{i+2}',f'{i+1}.Parameter')
+            FreeCAD.ActiveDocument.Parameters.setStyle(f'A{i+2}:A{i+2}', 'bold', 'add')
+
+        self.paramsheet.set('B1','Parameter Name')
+        FreeCAD.ActiveDocument.Parameters.setStyle('B1:B1', 'bold', 'add')
+        self.paramsheet.set('C1','Min Value')
+        FreeCAD.ActiveDocument.Parameters.setStyle('C1:C1', 'bold', 'add')
+        self.paramsheet.set('D1','Max Value')
+        FreeCAD.ActiveDocument.Parameters.setStyle('D1:D1', 'bold', 'add')
+        self.paramsheet.set('E1','Number of Generations')
+        FreeCAD.ActiveDocument.Parameters.setStyle('E1:E1', 'bold', 'add')
+        self.paramsheet.set('G1','Mesh Size')
+        FreeCAD.ActiveDocument.Parameters.setStyle('G1:G1', 'bold', 'add')
+        FreeCAD.ActiveDocument.recompute() 
+
+FreeCADGui.addCommand('Initiate', InitiateCommand())
